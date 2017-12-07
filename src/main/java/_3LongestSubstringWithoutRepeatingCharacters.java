a5ee2a20f6ae3bc34900af7591d5487e9f0e3299
崇爱甄 <18363999928@163.com>
2017-12-07 06:32:26 -0600

    新增第三道题的部分注释

--- src/main/java/_3LongestSubstringWithoutRepeatingCharacters.java
+++ src/main/java/_3LongestSubstringWithoutRepeatingCharacters.java
@@ -26,11 +26,16 @@ public class _3LongestSubstringWithoutRepeatingCharacters {
         if (s.length()==0) return 0;
         HashMap<Character, Integer> map = new HashMap<Character, Integer>();
         int max=0;
+        //对字符串进行遍历
         for (int i=0, j=0; i<s.length(); ++i){
+            //如果map中包含这个字符（）
             if (map.containsKey(s.charAt(i))){
+                //j为"j"和"map中存储的此字符的下标+1"的最大值
                 j = Math.max(j,map.get(s.charAt(i))+1);
             }
+            //map添加字符和字符的下标（只会保留最后一次字符出现的下标）
             map.put(s.charAt(i),i);
+            //max为"max"和"i-j+1"的最大值
             max = Math.max(max,i-j+1);
         }
         return max;
