de97515514afd8ff554874b3ad9a3d89c706a89c
崇爱甄 <18363999928@163.com>
2017-12-07 06:58:42 -0600

    完善对题目意思的理解

--- src/main/java/_4MedianofTwoSortedArrays.java
+++ src/main/java/_4MedianofTwoSortedArrays.java
@@ -1,14 +1,19 @@
 
 /*
-There are two sorted arrays nums1 and nums2 of size m and n respectively.
+There are two sorted arrays（有序数组） nums1 and nums2 of size m and n respectively（分别，个别）.
 
-Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+Find the median（中位数） of the two sorted arrays. The overall（整体的） run time complexity（运行时间复杂度） should be O(log (m+n))。
+
+中位数的定义为：
+对于有限的数集，可以通过把所有观察值高低排序后找出正中间的一个作为中位数。如果观察值有偶数个，通常取最中间的两个数值的平均数作为中位数
 
 Example 1:
 nums1 = [1, 3]
 nums2 = [2]
 
 The median is 2.0
+
+
 Example 2:
 nums1 = [1, 2]
 nums2 = [3, 4]
